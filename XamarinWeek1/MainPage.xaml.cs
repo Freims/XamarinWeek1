f052ad65476e23567296e3a4b53cf1693b1d68c5
marcosrrg9813 <marcosrrg9813@gmail.com>
2019-08-15 14:50:38 -0400

    Add project files.

---
+++ XamarinWeek1/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms; 
+
+namespace XamarinWeek1
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void  LoginButtonClicked(object sender, EventArgs e)
+        {
+            string password = passwordInput.Text;
+            string username = usernameInput.Text;
+
+          if(String.IsNullOrEmpty(password) || String.IsNullOrEmpty(username))
+                await DisplayAlert("Username and password fields are required", "Please check and try again.", "Accept");
+          else
+                await DisplayAlert("Login sucessful", $"Welcome {username}!", "Ok");
+
+        }
+    }
+}
