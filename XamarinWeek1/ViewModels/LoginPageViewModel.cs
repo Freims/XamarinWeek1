6d9c8aa82bc0a4812c4541eabfca1e1ca69efa39
marcosrrg9813 <marcosrrg9813@gmail.com>
2019-08-30 17:19:59 -0400

    Updating for HomeWork2

--- XamarinWeek1/ViewModels/LoginPageViewModel.cs
+++ XamarinWeek1/ViewModels/LoginPageViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 using System.Windows.Input;
 using Xamarin.Forms;
@@ -7,27 +8,43 @@ using XamarinWeek1.Models;
 
 namespace XamarinWeek1.ViewModels
 {
-    class LoginPageViewModel
+    class LoginPageViewModel : INotifyPropertyChanged
     {
         public ICommand LoginCommand { get; set; }
         public User User { get; set; }
+        public string ValidationMessage { get; set; }
+        public bool ValidationLabelVisible { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
 
          
         public LoginPageViewModel()
         {
             User = new User();
+            ValidationLabelVisible = false;
+            
             LoginCommand = new Command(async() =>
             {
                 ValidateLoginForm();
             }
             );
+
         }
         public async void  ValidateLoginForm()
         {
+        
+            ValidationLabelVisible = false;
             if (String.IsNullOrEmpty(User.Username) || String.IsNullOrEmpty(User.Password))
-                await App.Current.MainPage.DisplayAlert("Username and password fields are required", "Please check and try again.", "Accept");
+            {
+                ValidationMessage = "Both fields can't be left blank. Please check.";
+            }
             else
+            {
                 await App.Current.MainPage.DisplayAlert("Login sucessful", $"Welcome {User.Username}!", "Ok");
+            }
+
         }
+
+        
     }
 }
